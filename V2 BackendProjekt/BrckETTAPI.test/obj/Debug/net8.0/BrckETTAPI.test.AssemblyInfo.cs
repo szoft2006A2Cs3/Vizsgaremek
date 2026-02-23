e4bc74bcaf23fbb40fd23cfc4e6e1919310bc990
AgostonFeher <feher.agoston@hbsz.edu.hu>
2026-02-23 23:06:47 +0100

    fixes

--- V2 BackendProjekt/BrckETTAPI.test/obj/Debug/net8.0/BrckETTAPI.test.AssemblyInfo.cs
+++ V2 BackendProjekt/BrckETTAPI.test/obj/Debug/net8.0/BrckETTAPI.test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrckETTAPI.test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00912ac4ab588932e3a7a16772efd0075f4f8b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183de68ceb25cd535227d4c869b9937fcbd14a1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrckETTAPI.test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrckETTAPI.test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
