4933ee2d547b99e9793359598b8bb29fab3f7e4f
FulopLaszlohbsz <fulop.laszlo.andras@hbsz.edu.hu>
2026-02-26 08:03:40 +0100

    Update CalendarView.jsx

--- FINAL REACT/BrckETT/src/CalendarView.jsx
+++ FINAL REACT/BrckETT/src/CalendarView.jsx
@@ -7,12 +7,6 @@ import { useState } from 'react';
 export default function CalendarView({schedulesList}) {
     const [selectedDate, setSelectedDate] = useState(null);
     const [events, setEvents] = useState({});
-<<<<<<< HEAD
-    const [selectedSchedule, setSelectedSchedule] = useState(0);
-
-    function renderScheduleList() {
-      const numbers = [1, 2, 3, 4, 5];
-=======
 
     const [selectedSchedule, setSelectedSchedule] = useState(schedulesList[0]);
     const navigate = useNavigate(); 
@@ -59,7 +53,6 @@ export default function CalendarView({schedulesList}) {
     }
 
     function renderScheduleList() {
->>>>>>> f76390a257aef87bc513786c7fd352c939a563b4
 
       return (
         <div className="leftSide-list">
@@ -79,27 +72,11 @@ export default function CalendarView({schedulesList}) {
             <div id='render-schedule-list-here' className='calendarView-leftSide'>
                 {renderScheduleList()}
             </div>
-<<<<<<< HEAD
-            <div className='calendarView-rightSide'>
-                {selectedDate === null ? (
-                    <Calendar
-                        events={events}
-                        onSelectDate={setSelectedDate}
-                    />
-                ) : (
-                    <DayView
-                        date={selectedDate}
-                        events={events}
-                        setEvents={setEvents}
-                        onBack={() => setSelectedDate(null)}
-                />)}
-=======
 
 
 
             <div id='render-calendar-here' className='calendarView-rightSide'>
                 {renderCalendar()}
->>>>>>> f76390a257aef87bc513786c7fd352c939a563b4
             </div>
         </div>
 )}
